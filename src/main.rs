dc90c3830b2b8a8aed9eb836ac9ba8ddd4e2d852
Leon <12817042+thedevleon@users.noreply.github.com>
2024-01-26 15:12:34 +0100

    feat: wip of led support

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,21 @@
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp32c6_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*};
+use esp32c6_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, rmt::Rmt, IO};
 use esp_backtrace as _;
+use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
+use smart_leds::{
+    brightness,
+    gamma,
+    hsv::{hsv2rgb, Hsv},
+    SmartLedsWrite,
+};
 
 #[embassy_executor::task]
 async fn run() {
     loop {
         log::info!("Hello world from embassy using esp-hal-async!");
-        Timer::after(Duration::from_millis(1_000)).await;
+        Timer::after(Duration::from_millis(5_000)).await;
     }
 }
 
@@ -21,6 +28,10 @@ async fn main(spawner: Spawner) {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    // Configure RMT peripheral globally
+    let rmt = Rmt::new(peripherals.RMT, 80u32.MHz(), &clocks).unwrap();
 
     embassy::init(
         &clocks,
@@ -36,8 +47,31 @@ async fn main(spawner: Spawner) {
 
     spawner.spawn(run()).ok();
 
+    // We use one of the RMT channels to instantiate a `SmartLedsAdapter` which can
+    // be used directly with all `smart_led` implementations
+    let rmt_buffer = smartLedBuffer!(1);
+    let mut led = SmartLedsAdapter::new(rmt.channel0, io.pins.gpio8, rmt_buffer);
+    let mut color = Hsv {
+        hue: 0,
+        sat: 255,
+        val: 255,
+    };
+    let mut data;
+
     loop {
-        log::info!("Bing!");
-        Timer::after(Duration::from_millis(5_000)).await;
+        // Iterate over the rainbow!
+        for hue in 0..=255 {
+            color.hue = hue;
+            // Convert from the HSV color space (where we can easily transition from one
+            // color to the other) to the RGB color space that we can then send to the LED
+            data = [hsv2rgb(color)];
+            // When sending to the LED, we do a gamma correction first (see smart_leds
+            // documentation for details) and then limit the brightness to 10 out of 255 so
+            // that the output it's not too bright.
+            led.write(brightness(gamma(data.iter().cloned()), 10))
+                .unwrap();
+
+            Timer::after(Duration::from_millis(500)).await;
+        }
     }
 }
