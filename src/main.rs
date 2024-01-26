ebf12933278b1369a24aa3327c068ff5c9e6d273
Leon <12817042+thedevleon@users.noreply.github.com>
2024-01-26 18:10:07 +0100

    feat: wip led - now with separate task

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,78 @@
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp32c6_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, rmt::Rmt, IO};
+use embassy_sync::signal::Signal;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use esp32c6_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, rmt::Rmt, IO, gpio::GpioPin, gpio::Output, gpio::PushPull};
 use esp_backtrace as _;
 use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
 use smart_leds::{
-    brightness,
-    gamma,
-    hsv::{hsv2rgb, Hsv},
-    SmartLedsWrite,
+    brightness, gamma, SmartLedsWrite, RGB8
 };
+use rgb::RGBA8;
+
+enum LedAnimation {
+    SolidColor(RGBA8), // color
+    FadeOnOff(RGBA8, f32), // color, duration
+    FadeFromTo(RGBA8, RGBA8, f32), // color1, color2, duration 
+    Blink(RGBA8, f32, f32), // color, duration, interval,
+    BlinkBurst(RGBA8, u8, f32, f32), // color, bursts, duration, interval
+    Off,
+}
+
+struct AnimationState {
+    // TODO add state
+    new_color: RGB8,
+    new_brightness: u8,
+}
+
+// LED Animation Queue as https://docs.embassy.dev/embassy-sync/git/default/signal/struct.Signal.html
+static LED_ANIMATION_SIGNAL: Signal<CriticalSectionRawMutex, LedAnimation> = Signal::new();
 
 #[embassy_executor::task]
-async fn run() {
+async fn led_animator(mut led: SmartLedsAdapter<esp32c6_hal::rmt::Channel<0>, 0, 25>) { // TODO types of channel and gpio
+    let mut state: AnimationState = AnimationState {
+        new_color: RGB8{r: 0, g: 0, b: 0},
+        new_brightness: 0,
+    };
+    let mut current_led_animation = LedAnimation::SolidColor(RGBA8{r: 0, g: 0, b: 0, a: 0});
+
     loop {
-        log::info!("Hello world from embassy using esp-hal-async!");
-        Timer::after(Duration::from_millis(5_000)).await;
+        // get the new animation from the queue if one is available
+        if LED_ANIMATION_SIGNAL.signaled()
+        {
+            current_led_animation = LED_ANIMATION_SIGNAL.wait().await;
+        }
+        
+        match current_led_animation {
+            LedAnimation::SolidColor(color) => {
+                // TODO: set the color
+                //state.new_color = color;
+                state.new_color = RGB8{r: color.r, g: color.g, b: color.b};
+                state.new_brightness = color.a;
+    
+            },
+            LedAnimation::FadeOnOff(color, duration) => {
+                // TODO: fade to the color over the duration
+            },
+            LedAnimation::FadeFromTo(color1, color2, duration) => {
+                // TODO: fade from color1 to color2 over the duration
+            },
+            LedAnimation::Blink(color, duration, interval) => {
+                // TODO: blink the color over the duration with the interval
+            },
+            LedAnimation::BlinkBurst(color, bursts, duration, interval) => {
+                // TODO: blink the color over the duration with the interval
+            },
+            LedAnimation::Off => {
+                // TODO: turn off the led
+            },
+        }
+
+        led.write(gamma(brightness([state.new_color].iter().cloned(), state.new_brightness))).unwrap();
+
+        // 30 Hz
+        Timer::after(Duration::from_millis(33)).await;
     }
 }
 
@@ -30,48 +87,62 @@ async fn main(spawner: Spawner) {
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
-    // Configure RMT peripheral globally
-    let rmt = Rmt::new(peripherals.RMT, 80u32.MHz(), &clocks).unwrap();
-
-    embassy::init(
-        &clocks,
-        esp32c6_hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
-    );
-
     // setup logger
     // To change the log_level change the env section in .cargo/config.toml
     // or remove it and set ESP_LOGLEVEL manually before running cargo run
     // this requires a clean rebuild because of https://github.com/rust-lang/cargo/issues/10358
     esp_println::logger::init_logger_from_env();
-    log::info!("Logger is setup");
 
-    spawner.spawn(run()).ok();
+    // something feels odd with the clock setup... it's running a lot faster than it should
+    embassy::init(
+        &clocks,
+        esp32c6_hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
+    );
 
-    // We use one of the RMT channels to instantiate a `SmartLedsAdapter` which can
-    // be used directly with all `smart_led` implementations
+    let rmt = Rmt::new(peripherals.RMT, 80u32.MHz(), &clocks).unwrap();
     let rmt_buffer = smartLedBuffer!(1);
     let mut led = SmartLedsAdapter::new(rmt.channel0, io.pins.gpio8, rmt_buffer);
-    let mut color = Hsv {
-        hue: 0,
-        sat: 255,
-        val: 255,
-    };
-    let mut data;
+
+    spawner.spawn(led_animator(led)).ok();
+    // TODO: replace once we know the correct types for the channel and gpio
+    // spawner.spawn(led_animator(rmt.channel0, io.pins.gpio8)).ok();
 
     loop {
-        // Iterate over the rainbow!
-        for hue in 0..=255 {
-            color.hue = hue;
-            // Convert from the HSV color space (where we can easily transition from one
-            // color to the other) to the RGB color space that we can then send to the LED
-            data = [hsv2rgb(color)];
-            // When sending to the LED, we do a gamma correction first (see smart_leds
-            // documentation for details) and then limit the brightness to 10 out of 255 so
-            // that the output it's not too bright.
-            led.write(brightness(gamma(data.iter().cloned()), 10))
-                .unwrap();
+        // Try all the different animations
 
-            Timer::after(Duration::from_millis(500)).await;
-        }
+        // purple and blue
+        let color1 = RGBA8{r: 138, g: 43, b: 226, a: 128};
+        let color2 = RGBA8{r: 0, g: 0, b: 255, a: 128};
+
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::SolidColor(RGBA8{r: 255, g: 0, b: 0, a: 128}));
+        Timer::after(Duration::from_millis(50_000)).await;
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::SolidColor(RGBA8{r: 0, g: 255, b: 0, a: 128}));
+        Timer::after(Duration::from_millis(50_000)).await;
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::SolidColor(RGBA8{r: 0, g: 255, b: 155, a: 128}));
+        Timer::after(Duration::from_millis(50_000)).await;
+
+        // SolidColor
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::SolidColor(color1));
+        Timer::after(Duration::from_millis(100_000)).await;
+
+        // FadeOnOff
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::FadeOnOff(color1, 2.0));
+        Timer::after(Duration::from_millis(100_000)).await;
+
+        // FadeFromTo
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::FadeFromTo(color1, color2, 3.0));
+        Timer::after(Duration::from_millis(100_000)).await;
+
+        // Blink
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::Blink(color1, 0.5, 1.0));
+        Timer::after(Duration::from_millis(100_000)).await;
+
+        // BlinkBurst
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::BlinkBurst(color1, 3, 0.1, 1.0));
+        Timer::after(Duration::from_millis(100_000)).await;
+
+        // Off
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::Off);
+        Timer::after(Duration::from_millis(100_000)).await;
     }
 }
