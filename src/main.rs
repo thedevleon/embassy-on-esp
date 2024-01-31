639ed229af0951a04e9690579164e2e2dd46aa3a
Leon <12817042+thedevleon@users.noreply.github.com>
2024-01-31 23:39:16 +0100

    feat: FadeOnOff

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 #![feature(type_alias_impl_trait)]
 
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer, Ticker};
+use embassy_time::{Duration, Timer, Ticker, Instant};
 use embassy_sync::signal::Signal;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-use esp32c6_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, rmt::Rmt, IO, gpio::GpioPin, gpio::Output, gpio::PushPull};
+use esp32c6_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, rmt::Rmt, IO};
 use esp_backtrace as _;
 use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
 use smart_leds::{
@@ -16,30 +16,24 @@ use rgb::RGBA8;
 
 enum LedAnimation {
     SolidColor(RGBA8), // color
-    FadeOnOff(RGBA8, f32), // color, duration
-    FadeFromTo(RGBA8, RGBA8, f32), // color1, color2, duration 
-    Blink(RGBA8, f32, f32), // color, duration, interval,
-    BlinkBurst(RGBA8, u8, f32, f32), // color, bursts, duration, interval
+    FadeOnOff(RGBA8, u64), // color, duration
+    FadeFromTo(RGBA8, RGBA8, u64), // color1, color2, duration 
+    Blink(RGBA8, u64, u64), // color, duration, interval,
+    BlinkBurst(RGBA8, u8, u64, u64), // color, bursts, duration, interval
     Off,
-}
-
-struct AnimationState {
-    // TODO add state
-    new_color: RGB8,
-    new_brightness: u8,
 }
 
 // LED Animation Queue as https://docs.embassy.dev/embassy-sync/git/default/signal/struct.Signal.html
 static LED_ANIMATION_SIGNAL: Signal<CriticalSectionRawMutex, LedAnimation> = Signal::new();
 
-#[embassy_executor::task]
-async fn led_animator(mut led: SmartLedsAdapter<esp32c6_hal::rmt::Channel<0>, 0, 25>) { // TODO types of channel and gpio
-    let mut ticker = Ticker::every(Duration::from_millis(33)); // 30 Hz
-    let mut state: AnimationState = AnimationState {
-        new_color: RGB8{r: 0, g: 0, b: 0},
-        new_brightness: 0,
-    };
+#[embassy_executor::task] // Note: embassy does not yet support generics in tasks
+async fn led_animator(mut led: SmartLedsAdapter<esp32c6_hal::rmt::Channel<0>, 0, 25>) {
+    let ticker_duration = Duration::from_millis(33); // 30 Hz
+    let mut ticker = Ticker::every(ticker_duration.clone());
+    let mut start_time: Instant = Instant::now();
     let mut current_led_animation = LedAnimation::SolidColor(RGBA8{r: 0, g: 0, b: 0, a: 0});
+    let mut current_color = RGB8{r: 0, g: 0, b: 0};
+    let mut current_brightness: u8 = 0;
 
     loop {
         // get the new animation from the queue if one is available
@@ -47,17 +41,33 @@ async fn led_animator(mut led: SmartLedsAdapter<esp32c6_hal::rmt::Channel<0>, 0,
         {
             current_led_animation = LED_ANIMATION_SIGNAL.wait().await;
         }
+
+        let now = Instant::now();
         
         match current_led_animation {
             LedAnimation::SolidColor(color) => {
-                // TODO: set the color
-                //state.new_color = color;
-                state.new_color = RGB8{r: color.r, g: color.g, b: color.b};
-                state.new_brightness = color.a;
+                current_color = RGB8{r: color.r, g: color.g, b: color.b};
+                current_brightness = color.a;
     
             },
             LedAnimation::FadeOnOff(color, duration) => {
-                // TODO: fade to the color over the duration
+                let mut progress = (now - start_time).as_millis() as f32 / duration as f32;
+
+                // wrap around
+                if progress > 1.0 {
+                    start_time = now;
+                    progress = progress - 1.0;
+                }
+
+                current_color = RGB8{r: color.r, g: color.g, b: color.b};
+
+                if progress < 0.5 {
+                    // Fade On
+                    current_brightness = (color.a as f32 * (progress * 2.0)) as u8; 
+                } else if progress > 0.5 {
+                    // Fade Off
+                    current_brightness = (color.a as f32 * (1.0 - ((progress - 0.5) * 2.0))) as u8;
+                }
             },
             LedAnimation::FadeFromTo(color1, color2, duration) => {
                 // TODO: fade from color1 to color2 over the duration
@@ -73,7 +83,9 @@ async fn led_animator(mut led: SmartLedsAdapter<esp32c6_hal::rmt::Channel<0>, 0,
             },
         }
 
-        led.write(gamma(brightness([state.new_color].iter().cloned(), state.new_brightness))).unwrap();
+        //log::info!("color: {:?}, brightness: {}", current_color, current_brightness);
+
+        led.write(gamma(brightness([current_color].iter().cloned(), current_brightness))).unwrap();
 
         ticker.next().await;
     }
@@ -105,16 +117,13 @@ async fn main(spawner: Spawner) {
     let led = SmartLedsAdapter::new(rmt.channel0, io.pins.gpio8, rmt_buffer);
 
     spawner.spawn(led_animator(led)).ok();
-    // TODO: replace once we know the correct types for the channel and gpio
-    // spawner.spawn(led_animator(rmt.channel0, io.pins.gpio8)).ok();
 
+    // Try all the different animations
     loop {
-        // Try all the different animations
 
         // purple and blue
-        let color1 = RGBA8{r: 138, g: 43, b: 226, a: 128};
-        let color2 = RGBA8{r: 0, g: 0, b: 255, a: 128};
-
+        let color1 = RGBA8{r: 138, g: 43, b: 226, a: 255};
+        let color2 = RGBA8{r: 0, g: 0, b: 255, a: 255};
 
         log::info!("Testing: RGB");
         LED_ANIMATION_SIGNAL.signal(LedAnimation::SolidColor(RGBA8{r: 255, g: 0, b: 0, a: 128}));
@@ -131,22 +140,22 @@ async fn main(spawner: Spawner) {
 
         // FadeOnOff
         log::info!("Testing: FadeOnOff");
-        LED_ANIMATION_SIGNAL.signal(LedAnimation::FadeOnOff(color1, 2.0));
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::FadeOnOff(color1, 2_000));
         Timer::after(Duration::from_millis(10_000)).await;
 
         // FadeFromTo
         log::info!("Testing: FadeFromTo");
-        LED_ANIMATION_SIGNAL.signal(LedAnimation::FadeFromTo(color1, color2, 3.0));
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::FadeFromTo(color1, color2, 3_000));
         Timer::after(Duration::from_millis(10_000)).await;
 
         // Blink
         log::info!("Testing: Blink");
-        LED_ANIMATION_SIGNAL.signal(LedAnimation::Blink(color1, 0.5, 1.0));
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::Blink(color1, 500, 1_000));
         Timer::after(Duration::from_millis(10_000)).await;
 
         // BlinkBurst
         log::info!("Testing: BlinkBurst");
-        LED_ANIMATION_SIGNAL.signal(LedAnimation::BlinkBurst(color1, 3, 0.1, 1.0));
+        LED_ANIMATION_SIGNAL.signal(LedAnimation::BlinkBurst(color1, 3, 100, 1_000));
         Timer::after(Duration::from_millis(10_000)).await;
 
         // Off
